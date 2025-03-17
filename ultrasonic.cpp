873069b67069ec88a9553be1a81835c6e2bf2f39
Mohamed-Fathy-K <m.kotb819@gmail.com>
2025-03-17 05:59:19 +0200

    Uploading Ultrasonic Driver

--- ultrasonic.cpp
+++ ultrasonic.cpp
@@ -1,2 +1,34 @@
-#include "ultrasonic.h"
 #include <Arduino.h>
+#include "ultrasonic.h"
+#include "ERROR_STATE.h"
+
+void UltrasonicInit()
+{
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+f32 UltrasonicCalculateDistance() {
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  u16 duration = pulseIn(ECHO_PIN, HIGH);
+  f32 distanceInCm = (duration * 0.0343) / 2;
+  if(distanceInCm > 333) // for below 6cm 
+  {
+    distanceInCm = 0;
+  }
+  return distanceInCm;
+}
+
+void UltrasonicPrintDistance() {
+  f32 distance = UltrasonicCalculateDistance();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
+
--- ultrasonic.h
+++ ultrasonic.h
@@ -1,7 +1,8 @@
 #ifndef ULTRASONIC_H
 #define ULTRASONIC_H
 
-
+#define TRIG_PIN 5
+#define ECHO_PIN 18
 
 
 #endif
